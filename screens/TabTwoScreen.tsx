e1f4f4540d1261ec96fd1531dc60ce5df6060f13
Daniel Anthony <danskyjs@icloud.com>
2022-02-06 16:04:09 -0300

    test: testing up

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,17 +1,13 @@
 import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { View } from '../components/Themed';
 
-export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
-  );
-}
+const TabTwoScreen = () => (
+  <View style={styles.container}>
+    <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -29,3 +25,5 @@ const styles = StyleSheet.create({
     width: '80%',
   },
 });
+
+export default TabTwoScreen;
